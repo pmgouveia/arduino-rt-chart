1d042f3fdb8f34efdccb54e084a8ed092bec798b
info@eempo.net <info@eempo.net>
2016-05-04 16:23:10 +0100

    adicionar hora

--- serial2file.py
+++ serial2file.py
@@ -1,26 +1,45 @@
 import serial
 import sys
-
+import datetime
+from datetime import datetime
 ## args... which serial? which sensor? 
 
 if len(sys.argv) < 2:
     sys.stderr.write('Utilizacao: python2 serial2file.py /dev/ttyUSBX modulo\n')
     sys.exit(1)
     
-ser = serial.Serial(sys.argv[1], 9600)
-#try to open all the ports...
+arduino = serial.Serial(sys.argv[1], 9600) # abre a porta do arduino
+
 while True:
+	agora = datetime.now()
+	
+	hora = str(agora.hour) + "h" + str(agora.minute) + "m" + str(agora.second) + "s;"
 	
 	try:
-		x = ser.readline()
-		# x = x.rstrip()
-		sys.stdout.write(x)
-		fo = open(sys.argv[2]+".dat", "a+")
-		fo.write(x)
-		fo.close()
-	except Exception:
+		#ler linha do serial(arduino) e guardar na variavel linhaSerial
+		linhaSerial = arduino.readline()
+		
+		linhaSerial = hora+linhaSerial
+        		
+		#imprime para o ecra o que recebeu
+		sys.stdout.write(linhaSerial) 
+		
+		# abre o ficheiro para escrever... argv[2] segundo argumento
+		ficheiroLog = open(sys.argv[2]+".dat", "a+")
+		
+		# escreve a linha no ficheiro
+		ficheiroLog.write(linhaSerial)
+		
+		# fecha o ficheiro
+		ficheiroLog.close()
+		#print "adeus"
+        except Exception:
 		print "Excepcao.... "
 		pass
+		
+		
+		
+	    
 	
 
 """
